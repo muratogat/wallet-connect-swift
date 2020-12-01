970d58ba02e2b2fc3158b348ffa3461a13e79ba4
Murat Ögat <murat@aktionariat.com>
2020-12-01 01:59:03 +0100

    cleanup

--- WalletConnect/WCInteractor.swift
+++ WalletConnect/WCInteractor.swift
@@ -22,7 +22,7 @@ public enum WCInteractorState {
     case disconnected
 }
 
-open class WCInteractor {
+public class WCInteractor {
     public let bridgeURL: URL
     public private(set) var state: WCInteractorState
     public var sessions: [WCSession]
@@ -31,9 +31,6 @@ open class WCInteractor {
     
     // incoming event handlers
     public var onSessionRequest: SessionRequestClosure?
-    public var onDisconnect: DisconnectClosure?
-    public var onError: ErrorClosure?
-    public var onCustomRequest: CustomRequestClosure?
     public var onSign: EthSignClosure?
     public var onTransaction: EthTransactionClosure?
 
@@ -46,6 +43,7 @@ open class WCInteractor {
     private weak var keepAliveTimer: Timer?
     private weak var sessionTimer: Timer?
     private let sessionRequestTimeout: TimeInterval = 20
+    private var disconnectExpected: Bool = false
 
     // Socket Init and Management
     public init(bridgeURL: URL) {
@@ -86,17 +84,23 @@ open class WCInteractor {
         }
     }
 
-    open func pause() {
+    public func pause() {
+        self.disconnectExpected = true
+        
         state = .paused
         socket.disconnect(forceTimeout: nil, closeCode: CloseCode.goingAway.rawValue)
     }
 
-    open func resume() {
+    public func resume() {
+        self.disconnectExpected = false
+        
         socket.connect()
         state = .connecting
     }
 
-    open func disconnect() {
+    public func disconnect() {
+        self.disconnectExpected = true
+        
         stopTimers()
 
         socket.disconnect()
@@ -104,8 +108,6 @@ open class WCInteractor {
 
         connectResolver = nil
         handshakeId = -1
-
-        // WCSessionStore.clear(session.topic)
     }
     
     // Connects a new session to this interactor. A session has both session.topic and clientId subscriptions.
@@ -125,33 +127,36 @@ open class WCInteractor {
         return nil
     }
     
-    open func approveSession(session: WCSession, accounts: [String], chainId: Int) -> Promise<Void> {
+    public func approveSession(session: WCSession, accounts: [String], chainId: Int) {
         guard handshakeId > 0 else {
-            return Promise(error: WCError.sessionInvalid)
+            WCLog("Invalid Session")
+            return
         }
         
-        let result = WCApproveSessionResponse(approved: true,
-                                              chainId: chainId,
-                                              accounts: accounts,
-                                              peerId: session.clientId,
-                                              peerMeta: clientMeta)
+        let result = WCApproveSessionResponse(approved: true, chainId: chainId, accounts: accounts, peerId: session.clientId, peerMeta: clientMeta)
         let response = JSONRPCResponse(id: handshakeId, result: result)
-        return encryptAndSend(session: session, data: response.encoded)
+        
+        encryptAndSend(session: session, data: response.encoded).cauterize()
+        
+        WCSessionStore.store(session)
     }
 
-    open func rejectSession(session: WCSession, _ message: String = "Session Rejected") -> Promise<Void> {
+    public func rejectSession(session: WCSession, _ message: String = "Session Rejected") {
         guard handshakeId > 0 else {
-            return Promise(error: WCError.sessionInvalid)
+            WCLog("Invalid Session")
+            return
         }
+        
         let response = JSONRPCErrorResponse(id: handshakeId, error: JSONRPCError(code: -32000, message: message))
-        return encryptAndSend(session: session, data: response.encoded)
+        encryptAndSend(session: session, data: response.encoded).cauterize()
     }
 
-    open func disconnectSession(session: WCSession) -> Promise<Void> {
+    public func disconnectSession(session: WCSession) {
         let result = WCSessionUpdateParam(approved: false, chainId: nil, accounts: nil)
         let response = JSONRPCRequest(id: generateId(), method: WCEvent.sessionUpdate.rawValue, params: [result])
-        return encryptAndSend(session: session, data: response.encoded)
-            .map { [weak self] in
+        
+        encryptAndSend(session: session, data: response.encoded)
+            .done { [weak self] in
                 // Remove from sessions array. Removed from session store elsewhere
                 if let index = self?.sessions.firstIndex(of: session) {
                     self?.sessions.remove(at: index)
@@ -162,16 +167,174 @@ open class WCInteractor {
                     self?.disconnect()
                 }
             }
+            .cauterize()
     }
 
-    
-    // Request handling. //
+    // Approves the received request with the appropriate payload
+    // This is called after user confirmation
+    // ethSign : expects signed message
+    // ethSignTransaction: expects signed transaction
+    // ethSendTransaction: expects txid of signed and sent transaction
+    public func approveRequest<T: Codable>(id: Int64, session: WCSession, result: T) -> Promise<Void> {
+        let response = JSONRPCResponse(id: id, result: result)
+        return encryptAndSend(session: session, data: response.encoded)
+    }
+
+    public func rejectRequest(id: Int64, session: WCSession, message: String) -> Promise<Void> {
+        let response = JSONRPCErrorResponse(id: id, error: JSONRPCError(code: -32000, message: message))
+        return encryptAndSend(session: session, data: response.encoded)
+    }
+}
+
+// MARK: internal funcs
+extension WCInteractor {
+    private func subscribe(topic: String) {
+        let message = WCSocketMessage(topic: topic, type: .sub, payload: "")
+        let data = try! JSONEncoder().encode(message)
+        socket.write(data: data)
+        WCLog("==> subscribe: \(String(data: data, encoding: .utf8)!)")
+    }
+
+    private func encryptAndSend(session: WCSession, data: Data) -> Promise<Void> {
+        WCLog("==> Encrypt and Send: \(String(data: data, encoding: .utf8)!) ")
+        let encoder = JSONEncoder()
+        let payload = try! WCEncryptor.encrypt(data: data, with: session.key)
+        let payloadString = encoder.encodeAsUTF8(payload)
+        let message = WCSocketMessage(topic: session.peerId ?? session.topic, type: .pub, payload: payloadString)
+        let data = message.encoded
+        return Promise { seal in
+            socket.write(data: data) {
+                // WCLog("==> sent \(String(data: data, encoding: .utf8)!) ")
+                seal.fulfill(())
+            }
+        }
+    }
+
+    // Murat - Custom keep-alive
+    private func setupKeepAlive() {
+        // Murat - Don't add more timers. Invalidate first if exists.
+        if (pingTimer?.isValid ?? false) {
+            pingTimer?.invalidate()
+        }
+        
+        if (keepAliveTimer?.isValid ?? false) {
+            keepAliveTimer?.invalidate()
+        }
+        
+        keepAliveTimer = Timer.scheduledTimer(withTimeInterval: 17, repeats: true) { [weak socket] _ in
+            if (!socket!.isConnected) {
+                WCLog("Socket Disconnected. Reconnecting.")
+                socket?.connect()
+                self.state = .connecting
+            }
+        }
+        
+        pingTimer = Timer.scheduledTimer(withTimeInterval: 15, repeats: true) { [weak socket] _ in
+            WCLog("==> ping")
+            socket?.write(ping: Data())
+        }
+    }
+
+    private func stopTimers() {
+        pingTimer?.invalidate()
+        keepAliveTimer?.invalidate()
+        sessionTimer?.invalidate()
+    }
+
+    private func onSessionRequestTimeout() {
+        onDisconnect(error: WCError.sessionRequestTimeout)
+    }
+}
+
+// MARK: WebSocket event handler
+extension WCInteractor {
+    private func onConnect() {
+        WCLog("<== websocketDidConnect")
+
+        setupKeepAlive()
+        reconnectExistingSessions()
+
+        connectResolver?.fulfill(true)
+        connectResolver = nil
+
+        state = .connected
+    }
+
+    private func onDisconnect(error: Error?) {
+        WCLog("<== websocketDidDisconnect, error: \(error.debugDescription)")
+        
+        guard disconnectExpected else {
+            print("Unexpected Disconnect. Reconnecting.")
+            socket.connect()
+            state = .connecting
+            return
+        }
+
+        stopTimers()
+
+        if let error = error {
+            connectResolver?.reject(error)
+        } else {
+            connectResolver?.fulfill(false)
+        }
+
+        connectResolver = nil
+        self.disconnectExpected = false
+        state = .disconnected
+    }
+
+    private func onReceiveMessage(text: String) {
+        // Respond to ping
+        if (text == "ping") {
+            return socket.write(pong: Data())
+        }
+        
+        // Respond to real messages with payload
+        guard let (topic, payload) = WCEncryptionPayload.extract(text) else {
+            WCLog("Can't get payload of message: " + text)
+            return
+        }
+                
+        guard let session = getSessionByTopic(topic: topic) else {
+            WCLog("No session found for topic: " + topic)
+            return
+        }
+        
+        WCLog("<== Received msg for topic: \(topic) --- Peer: \(session.peerMeta?.url ?? "NOT SET YET")")
+        
+        do {
+            let decrypted = try WCEncryptor.decrypt(payload: payload, with: session.key)
+            
+            guard let json = try JSONSerialization.jsonObject(with: decrypted, options: []) as? [String: Any] else {
+                WCLog("Failed to deserialize payload.")
+                return
+            }
+            
+            WCLog("<== Decrypted Successfully: \(String(data: decrypted, encoding: .utf8)!)")
+            
+            guard let method = json["method"] as? String else {
+                WCLog("Failed to parse received method.")
+                return
+            }
+            
+            guard let event = WCEvent(rawValue: method) else {
+                WCLog("Unknown received method.")
+                return
+            }
+            
+            // Wow we made it here. Everything was correctly formatted and decrypted.
+            // Try handling it.
+            try handleEvent(event, session: session, decrypted: decrypted)
+        }
+        catch let error {
+            WCLog("==> onReceiveMessage error: \(error.localizedDescription)")
+        }
+    }
     
     // Handles the incoming message AFTER DECRYPTION
     // Decodes the decrypted message into Payload objects and triggers callbacks, if set
     // For example, showing the approval sheet to the user
     private func handleEvent(_ event: WCEvent, session: WCSession, decrypted: Data) throws {
-        
         switch event {
         case .sessionRequest:
             // topic == session.topic
@@ -188,7 +351,7 @@ open class WCInteractor {
             let request: JSONRPCRequest<[WCSessionUpdateParam]> = try event.decode(decrypted)
             guard let param = request.params.first else { throw WCError.badJSONRPCRequest }
             if param.approved == false {
-                disconnect()
+                disconnectSession(session: session)
             }
             
         case .ethSign, .ethPersonalSign:
@@ -207,165 +370,7 @@ open class WCInteractor {
             let request: JSONRPCRequest<[WCEthereumTransaction]> = try event.decode(decrypted)
             guard !request.params.isEmpty else { throw WCError.badJSONRPCRequest }
             onTransaction?(request.id, session, event, request.params[0])
-            
-        default:
-            break
         }
     }
     
-    // Approves the received request with the appropriate payload
-    // This is called after user confirmation
-    // ethSign : expects signed message
-    // ethSignTransaction: expects signed transaction
-    // ethSendTransaction: expects txid of signed and sent transaction
-    open func approveRequest<T: Codable>(id: Int64, session: WCSession, result: T) -> Promise<Void> {
-        let response = JSONRPCResponse(id: id, result: result)
-        return encryptAndSend(session: session, data: response.encoded)
-    }
-
-    open func rejectRequest(id: Int64, session: WCSession, message: String) -> Promise<Void> {
-        let response = JSONRPCErrorResponse(id: id, error: JSONRPCError(code: -32000, message: message))
-        return encryptAndSend(session: session, data: response.encoded)
-    }
-}
-
-// MARK: internal funcs
-extension WCInteractor {
-    public func subscribe(topic: String) {
-        let message = WCSocketMessage(topic: topic, type: .sub, payload: "")
-        let data = try! JSONEncoder().encode(message)
-        socket.write(data: data)
-        WCLog("==> subscribe: \(String(data: data, encoding: .utf8)!)")
-    }
-
-    private func encryptAndSend(session: WCSession, data: Data) -> Promise<Void> {
-        WCLog("==> encrypt: \(String(data: data, encoding: .utf8)!) ")
-        let encoder = JSONEncoder()
-        let payload = try! WCEncryptor.encrypt(data: data, with: session.key)
-        let payloadString = encoder.encodeAsUTF8(payload)
-        let message = WCSocketMessage(topic: session.peerId ?? session.topic, type: .pub, payload: payloadString)
-        let data = message.encoded
-        return Promise { seal in
-            socket.write(data: data) {
-                WCLog("==> sent \(String(data: data, encoding: .utf8)!) ")
-                seal.fulfill(())
-            }
-        }
-    }
-
-    
-    // Murat - Fully custom keep alive
-    private func setupKeepAlive() {
-        // Murat - Don't add more timers. Invalidate first if exists.
-        if (pingTimer?.isValid ?? false) {
-            pingTimer?.invalidate()
-        }
-        
-        if (keepAliveTimer?.isValid ?? false) {
-            keepAliveTimer?.invalidate()
-        }
-        
-        keepAliveTimer = Timer.scheduledTimer(withTimeInterval: 131, repeats: true) { [weak socket] _ in
-            WCLog("-- keepAlive ---")
-            self.pause()
-            self.resume()
-        }
-        
-        pingTimer = Timer.scheduledTimer(withTimeInterval: 23, repeats: true) { [weak socket] _ in
-            // WCLog("==> ping")
-            socket?.write(ping: Data())
-        }
-    }
-
-    /*
-    private func connectExistingSessions() {
-        // check if it's an existing session
-        if let existing = WCSessionStore.load(session.topic), existing.session == session {
-            peerId = existing.peerId
-            peerMeta = existing.peerMeta
-            return
-        }
-
-        // we only setup timer for new sessions
-        sessionTimer = Timer.scheduledTimer(withTimeInterval: sessionRequestTimeout, repeats: false) { [weak self] _ in
-            self?.onSessionRequestTimeout()
-        }
-    }
- */
-
-    private func stopTimers() {
-        pingTimer?.invalidate()
-        keepAliveTimer?.invalidate()
-        sessionTimer?.invalidate()
-    }
-
-    private func onSessionRequestTimeout() {
-        onDisconnect(error: WCError.sessionRequestTimeout)
-    }
-}
-
-// MARK: WebSocket event handler
-extension WCInteractor {
-    private func onConnect() {
-        WCLog("<== websocketDidConnect")
-
-        //setupPingTimer()
-        setupKeepAlive()
-        reconnectExistingSessions()
-
-        connectResolver?.fulfill(true)
-        connectResolver = nil
-
-        state = .connected
-    }
-
-    private func onDisconnect(error: Error?) {
-        WCLog("<== websocketDidDisconnect, error: \(error.debugDescription)")
-
-        stopTimers()
-
-        if let error = error {
-            connectResolver?.reject(error)
-        } else {
-            connectResolver?.fulfill(false)
-        }
-
-        connectResolver = nil
-        onDisconnect?(error)
-
-        state = .disconnected
-    }
-
-    private func onReceiveMessage(text: String) {
-        // handle ping in text format :(
-        if text == "ping" { return socket.write(pong: Data()) }
-        guard let (topic, payload) = WCEncryptionPayload.extract(text) else { return }
-        WCLog("<== Received msg for topic: \(topic)")
-        do {
-            guard let session = getSessionByTopic(topic: topic) else {
-                WCLog("No session found for topic: " + topic)
-                return
-            }
-            
-            let decrypted = try WCEncryptor.decrypt(payload: payload, with: session.key)
-            
-            guard let json = try JSONSerialization.jsonObject(with: decrypted, options: []) as? [String: Any] else {
-                throw WCError.badJSONRPCRequest
-            }
-            
-            // WCLog("<== Decrypted Successfully: \(String(data: decrypted, encoding: .utf8)!)")
-            
-            if let method = json["method"] as? String {
-                WCLog("<== Decrypted Method: \(method)")
-                if let event = WCEvent(rawValue: method) {
-                    try handleEvent(event, session: session, decrypted: decrypted)
-                } else if let id = json["id"] as? Int64 {
-                    onCustomRequest?(id, json)
-                }
-            }
-        } catch let error {
-            onError?(error)
-            WCLog("==> onReceiveMessage error: \(error.localizedDescription)")
-        }
-    }
 }
